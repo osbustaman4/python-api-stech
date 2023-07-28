d3ef084b7a50a8fb96cf1de1581862f467458ff0
Osvaldo Bustamante <osvaldo@stech.guru>
2023-07-28 16:01:35 +0000

    upload swagger

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from src.routes import ObjectDataRoutes
 
 app = Flask(__name__)
+
+# swagger configs
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name' : "Todo Lista Endpoints"
+    }
+)
+
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix = SWAGGER_URL)
+
 
 def init_app(config):
 
