5a962c17e0b34857f9073716ce24acfe79d04866
osbustaman <osvaldo.bustamante.n@gmail.com>
2023-08-04 11:32:25 -0400

    Update index.py

--- index.py
+++ index.py
@@ -4,5 +4,8 @@ from src import init_app
 configuration = configure['development']
 app = init_app(configuration)
 
+# Agregar esta línea para definir la variable 'application'
+application = app
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
